8bbfeb9be2ab843c7ef2dcd3b591bf595f37d5e8
oneeyeman1 <ikorot01@gmail.com>
2016-05-26 23:53:53 -0400

    Refactor editing DSN code

--- libodbc/database_odbc.cpp
+++ libodbc/database_odbc.cpp
@@ -150,6 +150,26 @@ bool ODBCDatabase::AddDsn(SQLHWND hwnd, const std::wstring &driver, std::vector<
     return result;
 }
 
+bool ODBCDatabase::EditDsn(SQLHWND hwnd, const std::wstring &driver, const std::wstring &dsn, std::vector<std::wstring> &errorMsg)
+{
+    bool result = true;
+    SQLWCHAR temp1[1024];
+    SQLWCHAR temp2[1024];
+    memset( temp1, 0, 1024 );
+    memset( temp2, 0, 1024 );
+    uc_to_str_cpy( temp1, driver );
+    uc_to_str_cpy( temp2, L"DSN=" );
+    uc_to_str_cpy( temp2, dsn );
+	BOOL ret= SQLConfigDataSource( hwnd, ODBC_CONFIG_DSN, temp1, temp2 );
+    if( !ret )
+    {
+        GetDSNErrorMessage( errorMsg );
+        if( !errorMsg.empty() )
+            result = false;
+    }
+    return result;
+}
+
 void ODBCDatabase::SetWindowHandle(SQLHWND handle)
 {
     m_handle = handle;
