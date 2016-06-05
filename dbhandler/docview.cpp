0e03ad6e2fff4b391b8ca33ea54a5cd10b508131
oneeyeman1 <ikorot01@gmail.com>
2016-06-05 14:20:20 -0400

    Add leak detecting ability

--- dbhandler/docview.cpp
+++ dbhandler/docview.cpp
@@ -26,6 +26,10 @@
     #error You must set wxUSE_DOC_VIEW_ARCHITECTURE to 1 in setup.h!
 #endif
 
+#ifdef __WXMSW__
+#include <vld.h>
+#endif
+
 #include <vector>
 #include <map>
 #include "wx/docview.h"
