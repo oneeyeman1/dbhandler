a3c47dc8a259f147025d973f88ba40fd57e9fa07
oneeyeman1 <ikorot01@gmail.com>
2016-06-26 12:55:32 -0400

    FIx the code with the new version of SQLite

--- libsqlite/database_sqlite.cpp
+++ libsqlite/database_sqlite.cpp
@@ -279,7 +279,7 @@ int SQLiteDatabase::GetTableListFromDb(std::vector<std::wstring> &errorMsg)
                                     break;
                                 }
                             }
-                            if( res2 != SQLITE_DONE )
+                            if( res3 != SQLITE_DONE )
                                 break;
                         }
                         else
@@ -290,7 +290,7 @@ int SQLiteDatabase::GetTableListFromDb(std::vector<std::wstring> &errorMsg)
                         }
                         sqlite3_reset( stmt3 );
                     }
-                    if( res1 == SQLITE_DONE && res2 == SQLITE_DONE )
+                    if( res1 == SQLITE_DONE && res3 == SQLITE_DONE )
                         pimpl->m_tables[m_catalog].push_back( Table( myconv.from_bytes( (const char *) tableName ), fields, foreign_keys ) );
                 }
                 else if( res == SQLITE_DONE )
