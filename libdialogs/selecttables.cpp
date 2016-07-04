35b7cd4d4dd1e3c9a02f97e9ee9b0d9ce2496468
oneeyeman1 <ikorot01@gmail.com>
2016-07-03 21:34:56 -0400

    Fix compile error on Xcode

--- libdialogs/selecttables.cpp
+++ libdialogs/selecttables.cpp
@@ -8,7 +8,7 @@
 // Example for compiling a multi file project under Linux using g++:
 //  g++ main.cpp $(wx-config --libs) $(wx-config --cxxflags) -o MyApp Dialog1.cpp Frame1.cpp
 //
-
+#include <string>
 #include "database.h"
 #include "selecttables.h"
 
