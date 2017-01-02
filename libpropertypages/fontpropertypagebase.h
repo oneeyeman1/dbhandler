249ae17c953232525641d6230cbef46db961f143
oneeyeman1 <ikorot01@gmail.com>
2017-01-01 21:58:11 -0500

    Fix compilation of font property page for all platforms

--- libpropertypages/fontpropertypagebase.h
+++ libpropertypages/fontpropertypagebase.h
@@ -18,8 +18,8 @@
  *   Free Software Foundation, Inc.,                                       *
  *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
  ***************************************************************************/
-#ifndef FONTPROPERTYPAGE_H
-#define FONTPROPERTYPAGE_H
+#ifndef FONTPROPERTYPAGEBASE_H
+#define FONTPROPERTYPAGEBASE_H
 
 class WXEXPORT CFontPropertyPageBase : public wxPanel
 {
