2c9e9483e33fc6c7d674881cbf8d91481f5dd828
oneeyeman1 <ikorot01@gmail.com>
2017-01-12 07:32:11 -0500

    Add Field Header property page code template

---
+++ libpropertypages/fieldheader.cpp
@@ -0,0 +1,34 @@
+// For compilers that support precompilation, includes "wx/wx.h".
+#include "wx/wxprec.h"
+
+#ifdef __BORLANDC__
+    #pragma hdrstop
+#endif
+
+#include "fieldheader.h"
+
+FieldHeader::FieldHeader(wxWindow *parent) : wxPanel( parent, wxID_ANY )
+{
+    m_label1 = new wxStaticText( this, wxID_ANY, _( "&Label:" ) );
+    m_label = new wxTextCtrl( this, wxID_ANY, "" );
+    m_label2 = new wxStaticText( this, wxID_ANY, _( "&Position: " ) );
+    m_labelPos = new wxComboBox( this, wxID_ANY );
+    m_label3 = new wxStaticText( this, wxID_ANY, _( "&Heading:" ) );
+    m_heading = new wxTextCtrl( this, wxID_ANY, "", wxDefaultPosition, wxDefaultSize, wxTE_MULTILINE );
+    m_headingPos = new wxComboBox( this, wxID_ANY );
+}
+
+FieldHeader::~FieldHeader(void)
+{
+}
+
+void FieldHeader::do_layout()
+{
+    wxBoxSizer *sizer1 = new wxBoxSizer( wxHORIZONTAL );
+    wxBoxSizer *sizer2 = new wxBoxSizer( wxVERTICAL );
+    sizer1->Add( 5, 5, 0, wxEXPAND, 0 );
+    sizer2->Add( 5, 5, 0, wxEXPAND, 0 );
+    sizer2->Add( 5, 5, 0, wxEXPAND, 0 );
+    sizer1->Add( 5, 5, 0, wxEXPAND, 0 );
+    SetSizer( sizer1 );
+}
---
+++ libpropertypages/fieldheader.h
@@ -0,0 +1,14 @@
+#pragma once
+class FieldHeader :	public wxPanel
+{
+public:
+    FieldHeader(wxWindow *parent);
+    ~FieldHeader(void);
+protected:
+    void do_layout();
+private:
+    wxStaticText *m_label1, *m_label2, *m_label3;
+    wxTextCtrl *m_label, *m_heading;
+    wxComboBox *m_labelPos, *m_headingPos;
+};
+
