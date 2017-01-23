da42364f3850282dab736e432b4169679eeaa687
Igor Korot <igor.korot>
2017-01-23 11:40:16 -0500

    Another formatting fix on SQLite implementation

--- libsqlite/database_sqlite.h
+++ libsqlite/database_sqlite.h
@@ -13,7 +13,7 @@ public:
     virtual int Connect(std::wstring selectedDSN, std::vector<std::wstring> &errorMsg);
     virtual int Disconnect(std::vector<std::wstring> &errorMsg);
     virtual int CreateIndex(const std::wstring &command, std::vector<std::wstring> &errorMsg);
-	virtual bool IsIndexExists(const std::wstring &indexName, const std::wstring &tableName, std::vector<std::wstring> &errorMsg);
+    virtual bool IsIndexExists(const std::wstring &indexName, const std::wstring &tableName, std::vector<std::wstring> &errorMsg);
     virtual int GetTableProperties(DatabaseTable *table, std::vector<std::wstring> &errorMsg);
     virtual int GetFieldProperties(const std::wstring &tableName, const std::wstring &schemaName, const std::wstring &fieldName, Field *table, std::vector<std::wstring> &errorMsg);
     virtual int SetTableProperties(const std::wstring &command, std::vector<std::wstring> &errorMsg);
