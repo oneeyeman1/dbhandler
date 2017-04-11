2b53d414b9a435cb7c72bd6455d5288a67c516d0
oneeyeman1 <ikorot01@gmail.com>
2017-04-11 00:05:31 -0400

    Revert "Fix compilation errors introduced at work"
    This reverts commit d7882a7229e2975a8aed39553843e56117287233.

--- libtablewindow/tablewindow.cpp
+++ libtablewindow/tablewindow.cpp
@@ -15,13 +15,11 @@
 #include <string>
 #include "wx/docmdi.h"
 #include "wx/docview.h"
-#include "wx/grid.h"
 #include "wx/dynlib.h"
 #include "wx/cmdproc.h"
 #include "database.h"
 #include "tabledoc.h"
 #include "tableview.h"
-#include "tablecanvas.h"
 
 #ifdef __WXMSW__
 WXDLLIMPEXP_BASE void wxSetInstance( HINSTANCE hInst );
@@ -91,7 +89,7 @@ extern "C" WXEXPORT void CreateDatabaseWindow(wxWindow *parent, wxDocManager *do
         new wxDocTemplate( docManager, "Drawing", "*.tbl", "", "tbl", "Table Doc", "Table View", CLASSINFO(TableDocument), CLASSINFO(TableView) );
     docManager->CreateDocument( "*.tbl", wxDOC_NEW | wxDOC_SILENT );
     dynamic_cast<TableDocument *>( docManager->GetCurrentDocument() )->SetDatabase( db );
-    new TableCanvas(dynamic_cast<TableView *>( docManager->GetCurrentView() ), wxDefaultPosition, table );
+    new TableCanvas(dynamic_cast<TableView *>( docManager->GetCurrentView() ), pt, table )
 }
 
 /*
