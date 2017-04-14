605e289b830fb1ca01a718263359e3ce0e4f6129
Korot <1265892163@mil>
2017-04-14 10:11:34 -0400

    Start implementing create/drop database

--- dbinterface/database.h
+++ dbinterface/database.h
@@ -192,6 +192,8 @@ protected:
 public:
     virtual ~Database() = 0;
     Impl &GetTableVector() { return *pimpl; };
+    virtual int CreateDatabase(const std::wstring &name, std::vector<std::wstring> &errorMsg) = 0;
+    virtual int DropDatabase(const std::wstring &name, std::vector<std::wstring> &errorMsg) = 0;
     virtual int Connect(std::wstring selectedDSN, std::vector<std::wstring> &errorMsg) = 0;
     virtual int Disconnect(std::vector<std::wstring> &errorMsg) = 0;
     virtual int CreateIndex(const std::wstring &command, std::vector<std::wstring> &errorMsg) = 0;
