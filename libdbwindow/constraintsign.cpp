b8c08c0b4d6f2031a675fa008fd00a15f3b40000
Korot <1265892163@mil>
2017-04-18 11:32:27 -0400

    Add template for constraint sign class

---
+++ libdbwindow/constraintsign.cpp
@@ -0,0 +1,30 @@
+/////////////////////////////////////////////////////////////////////////////
+// Name:        samples/docview/mainframe.cpp
+// Purpose:     DB Handler
+// Author:      Igor Korot
+// Created:     17 DEC 2015
+// Licence:     wxWindows licence
+/////////////////////////////////////////////////////////////////////////////
+
+// For compilers that support precompilation, includes "wx/wx.h".
+#include "wx/wxprec.h"
+
+#ifdef __BORLANDC__
+#pragma hdrstop
+#endif
+
+#ifndef WX_PRECOMP
+#include "wx/wx.h"
+#include "wx/mdi.h"
+#endif
+
+#include "wxsf/RectShape.h"
+#include "constraintsign.h"
+
+ConstraintSign::ConstraintSign()
+{
+}
+
+ConstraintSign::~ConstraintSign()
+{
+}
---
+++ libdbwindow/constraintsign.h
@@ -0,0 +1,10 @@
+#ifndef __CONSTRAINTSIGN__
+#define __CONSTRAINTSIGN__
+
+class ConstraintSign : public wxSFRectShape
+{
+public:
+    ConstraintSign();
+    virtual ~ConstraintSign();
+};
+#endif
