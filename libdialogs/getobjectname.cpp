64a4d7177a2af4e567abc13b987330f8be4ee9fe
Korot <1265892163@mil>
2017-04-19 13:02:14 -0400

    Add object selection dialog

---
+++ libdialogs/getobjectname.cpp
@@ -0,0 +1,60 @@
+#ifdef __BORLANDC__
+#pragma hdrstop
+#endif
+
+#ifndef WX_PRECOMP
+#include "wx/wx.h"
+#endif
+
+#include "getobjectname.h"
+
+GetObjectName::GetObjectName(wxWindow *parent, int id, const wxString &title, int objectId) : wxDialog( parent, id, title )
+{
+    m_panel = new wxPanel( this );
+    m_painterNameLabel = new wxStaticText( m_panel, wxID_ANY, "" );
+    if( objectId == QUERY )
+    {
+        SetTitle( _( "Query" ) );
+        m_painterNameLabel->SetLabel( _( "Query Name:" ) );
+    }
+    m_painterName = new wxTextCtrl( m_panel, wxID_ANY, "" );
+    m_objectList = new wxListCtrl( m_panel, wxID_ANY, wxDefaultPosition, wxDefaultSize, 0, NULL, wxLB_SINGLE | wxLB_ALWAYS_SB );
+    m_comments = new wxStaticText( m_panel, wxID_ANY, _( "&Comments" ) );
+    m_commentsText = new wxTextCtrl( m_panel, wxID_ANY, wxEmptyString, wxDefaultPosition, wxDefaultSize, wxTE_MULTILINE );
+    m_ok = new wxButton( m_panel, wxID_OK, _( "OK" ) );
+    m_cancel = new wxButton( m_panel, wxID_CANCEL, _( "Cancel" ) );
+    m_new = new wxButton( m_panel, wxID_ANY, _( "&New" ) );
+    m_browse = new wxButton( m_panel, wxID_ANY, _( "&Browse" ) );
+    m_help = new wxButton( m_panel, wxID_HELP, _( "&Help" ) );
+    set_properties();
+    do_layout();
+}
+
+void GetObjectName::set_properties()
+{
+    m_ok->SetDefault();
+}
+
+void GetObjectName::do_layout()
+{
+    wxBoxSizer *main = new wxBoxSizer( wxHORIZONTAL );
+    wxBoxSizer *sizer_1 = new wxBoxSizer( wxHORIZONTAL );
+    wxBoxSizer *sizer_2 = new wxBoxSizer( wxVERTICAL );
+    FlexGridSizer *sizer_3 = new FlexGridSizer( 2, 3, 0, 0 );
+    wxBoxSizer *sizer_4 = new wxBoxSizer( wxVERTICAL );
+    wxBoxSizer *sizer_5 = new wxBoxSizer( wxVERTICAL );
+    sizer_1->Add( 20, 20, 0, wxEXPAND, 0 );
+    sizer_2->Add( 20, 20, 0, wxEXPAND, 0 );
+    sizer_5->Add( m_painterNameLabel, 0, wxEXPAND, 0 );
+    sizer_5->Add( m_painterName, 0, wxEXPAND, 0 );
+    sizer_5->Add( 20, 10, 0, wxEXPAND, 0 );
+    sizer_2->Add( sizer_3, 0, wxEXPAND, 0 );
+    sizer_2->Add( 20, 20, 0, wxEXPAND, 0 );
+    sizer_1->Add( sizer_2, 0, wxEXPAND, 0 );
+    sizer_1->Add( 20, 20, 0, wxEXPAND, 0 );
+    m_panel->SetSizer( sizer_1 );
+    main->Add( m_panel, 0, wxEXPAND, 0 );
+    SetSizer( main );
+    main->Fit( this );
+    Layout();
+}
---
+++ libdialogs/getobjectname.h
@@ -0,0 +1,18 @@
+#ifndef __GETOBJECTNAME_H__
+#define __GETOBJECTNAME_H__
+
+class GetObjectName; public wxDialog
+{
+public:
+    GetObjectName(wxWindow *parent, int id, const wxString &title, int objectId);
+protected:
+    void set_properties();
+    void do_layout();
+private:
+    wxPanel *m_panel;
+    wxStaticText *m_painterNameLabel, *m_comments;
+    wxTextCtrl *m_painterName, *m_commentsText;
+    wxListCtrl *m_objectList;
+    wxButton *m_ok, *m_cancel, *m_new, *m_browse, *m_help;
+};
+#endif
