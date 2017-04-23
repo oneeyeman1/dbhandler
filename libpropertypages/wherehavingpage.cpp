f092c907cef5bd38050d7b1130693af3e1c95035
oneeyeman1 <ikorot01@gmail.com>
2017-04-23 16:38:56 -0400

    Continue implementing where/having page - creating the grid

--- libpropertypages/wherehavingpage.cpp
+++ libpropertypages/wherehavingpage.cpp
@@ -15,6 +15,39 @@
 
 WhereHavingPage::WhereHavingPage(wxWindow *parent) : wxPanel( parent )
 {
+    m_operatorChoices[0] = "=";
+    m_operatorChoices[1] = "<";
+    m_operatorChoices[2] = ">";
+    m_operatorChoices[3] = "<=";
+    m_operatorChoices[4] = ">=";
+    m_operatorChoices[5] = "<>";
+    m_operatorChoices[6] = "between";
+    m_operatorChoices[7] = "exists";
+    m_operatorChoices[8] = "in";
+    m_operatorChoices[9] = "like";
+    m_operatorChoices[10] = "not between";
+    m_operatorChoices[11] = "not exists";
+    m_operatorChoices[12] = "not in";
+    m_operatorChoices[13] = "not like";
+    m_operatorChoices[14] = "is";
+    m_operatorChoices[15] = "is not";
+    m_operatorChoices[16] = "= all";
+    m_operatorChoices[17] = "!= all";
+    m_operatorChoices[18] = "< all";
+    m_operatorChoices[19] = "> all";
+    m_operatorChoices[20] = "<= all";
+    m_operatorChoices[21] = ">= all";
+    m_operatorChoices[22] = "= any";
+    m_operatorChoices[23] = "< any";
+    m_operatorChoices[24] = "> any";
+    m_operatorChoices[25] = "<= any";
+    m_operatorChoices[26] = ">= any";
+    m_operatorChoices[27] = "!= any";
+    m_logicalChoices[0] = "And";
+    m_logicalChoices[1] = "Or";
+    m_grid = new wxGrid( this, wxID_ANY );
+    set_properties();
+    do_layout();
 }
 
 WhereHavingPage::~WhereHavingPage(void)
@@ -23,8 +56,20 @@ WhereHavingPage::~WhereHavingPage(void)
 
 void WhereHavingPage::set_properties()
 {
+    m_grid->CreateGrid( 0, 0 );
+    m_grid->HideRowLabels();
+    m_grid->SetColLabelValue( 0, _( "Column" ) );
+    m_grid->SetColLabelValue( 1, _( "Operator" ) );
+    m_grid->SetColLabelValue( 2, _( "Value" ) );
+    m_grid->SetColLabelValue( 3, _( "Logical" ) );
+/*    m_grid->SetCellEditor( 0, 0, new wxGridCellChoiceEditor() );
+    m_grid->SetCellEditor( 0, 1, new wxGridCellChoiceEditor( 28, m_operatorChoices ) );
+    m_grid->SetCellEditor( 0, 3, new wxGridCellChoiceEditor( 2, m_logicalChoices ) );*/
 }
 
 void WhereHavingPage::do_layout()
 {
+    wxBoxSizer *sizer = new wxBoxSizer( wxVERTICAL );
+    sizer->Add( m_grid, 1, wxEXPAND, 0 );
+    SetSizer( sizer );
 }
--- libpropertypages/wherehavingpage.h
+++ libpropertypages/wherehavingpage.h
@@ -9,5 +9,6 @@ protected:
     void set_properties();
 private:
     wxGrid *m_grid;
+    wxString m_operatorChoices[28], m_logicalChoices[2];
 };
 
