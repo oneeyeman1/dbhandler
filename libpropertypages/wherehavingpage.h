b38ac424a4f7d1c1dbeb1c983c7450d95d405f88
Igor Korot <igor.n.korot.ctr@mail.mil>
2017-04-25 12:42:44 -0400

    Sizing where/having grid horizontally

--- libpropertypages/wherehavingpage.h
+++ libpropertypages/wherehavingpage.h
@@ -5,10 +5,12 @@ public:
     WhereHavingPage(wxWindow *parent);
     ~WhereHavingPage(void);
     void AppendField(const std::wstring &field);
+    void OnSize(wxSizeEvent &event);
 protected:
     void do_layout();
     void set_properties();
 private:
+    int m_scrollbarWidth, m_operatorSize, m_logicalSize;
     wxGrid *m_grid;
     wxString m_operatorChoices[28], m_logicalChoices[2];
     std::vector<std::wstring> m_fields;
