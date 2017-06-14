154372365453119f59ad0509aa986bbaed1dea94
oneeyeman1 <ikorot01@gmail.com>
2017-06-14 18:51:23 -0400

    Properly create a context menu on where/having page

--- dbhandler/docview.h
+++ dbhandler/docview.h
@@ -111,6 +111,12 @@ wxDECLARE_APP(MyApp);
 #define wxID_SHOWLABELS           118
 #define wxID_SHOWCOMMENTS         119
 #define wxID_SHOWSQLTOOLBOX       120
+#define WHEREPAGECOLUMNS          194
+#define WHEREPAGEFUNCTIONS        195
+#define WHEREPAGEARGUMENTS        196
+#define WHEREPAGEVALUE            197
+#define WHEREPAGESELECT           198
+#define WHEREPAGECLEAR            199
 #define wxID_EDITCUTCOLUMN        200
 #define wxID_EDITCOPYCOLUMN       201
 #define wxID_EDITPASTECOLUMN      202
