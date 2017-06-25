54054b503bbfa3e68e6f11570cb882b5266695ee
oneeyeman1 <ikorot01@gmail.com>
2017-06-24 22:37:10 -0400

    Add RTTI to comment field shape

--- libdbwindow/commentfieldshape.cpp
+++ libdbwindow/commentfieldshape.cpp
@@ -2,11 +2,33 @@
 #include "wxsf/textshape.h"
 #include "commentfieldshape.h"
 
-CommentFieldShape::CommentFieldShape(Field *field)
+XS_IMPLEMENT_CLONABLE_CLASS(CommentFieldShape, wxSFTextShape);
+
+CommentFieldShape::CommentFieldShape() : wxSFTextShape()
+{
+    m_field = NULL;
+}
+
+CommentFieldShape::CommentFieldShape(Field *field) : wxSFTextShape()
 {
     m_field = field;
 }
 
+CommentFieldShape::CommentFieldShape(CommentFieldShape &shape)
+{
+    m_field = shape.m_field;
+}
+
 CommentFieldShape::~CommentFieldShape(void)
 {
+}
+
+const Field *CommentFieldShape::GetFieldForComment()
+{
+    return m_field;
+}
+
+void CommentFieldShape::SetField(Field *field)
+{
+    m_field = field;
 }
--- libdbwindow/commentfieldshape.h
+++ libdbwindow/commentfieldshape.h
@@ -2,8 +2,13 @@
 class CommentFieldShape : public wxSFTextShape
 {
 public:
+    XS_DECLARE_CLONABLE_CLASS(FieldShape);
+    CommentFieldShape();
     CommentFieldShape(Field *field);
+    CommentFieldShape(CommentFieldShape &shape);
     virtual ~CommentFieldShape(void);
+    const Field *GetFieldForComment();
+    void SetField(Field *field);
 private:
     Field *m_field;
 };
