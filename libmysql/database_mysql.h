80b44a43aa158afbd4d82dc68e6caafc25b42d89
oneeyeman1 <ikorot01@gmail.com>
2017-07-02 14:55:04 -0400

    Fix unused parameter

--- libmysql/database_mysql.h
+++ libmysql/database_mysql.h
@@ -16,7 +16,7 @@ public:
     virtual int DropDatabase(const std::wstring &name, std::vector<std::wstring> &errorMsg);
     int mySQLConnect();
     virtual int Connect(std::wstring selectedDSN, std::vector<std::wstring> &errorMsg);
-    virtual int Disconnect(std::vector<std::wstring> &WXUNUSED(errorMsg));
+    virtual int Disconnect(std::vector<std::wstring> &errorMsg);
     virtual int CreateIndex(const std::wstring &command, std::vector<std::wstring> &errorMsg);
     virtual bool IsIndexExists(const std::wstring &indexName, const std::wstring &schemaName, const std::wstring &tableName, std::vector<std::wstring> &errorMsg);
     virtual int GetTableProperties(DatabaseTable *table, std::vector<std::wstring> &errorMsg);
