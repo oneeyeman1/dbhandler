b21dbd197e8cde706bcad2659085c75f90fbee7c
Igor Korot <igor.korot>
2017-09-07 12:14:25 -0400

    Implement browse functionality in get object name dialog

--- libdialogs/getobjectname.h
+++ libdialogs/getobjectname.h
@@ -6,6 +6,8 @@ class GetObjectName : public wxDialog
 public:
     GetObjectName(wxWindow *parent, int id, const wxString &title, int objectId);
     void OnButtonNew(wxCommandEvent &event);
+    void OnButtonBrowse(wxCommandEvent &event);
+    const wxString &GetFileName();
 protected:
     void set_properties();
     void do_layout();
@@ -16,5 +18,6 @@ private:
     wxListCtrl *m_objectList;
     wxButton *m_ok, *m_cancel, *m_new, *m_browse, *m_help;
     int m_id;
+    wxString m_fileName;
 };
 #endif
