d76f25e7d593b0d14de2ec807988710bb2ba05f3
oneeyeman1 <ikorot01@gmail.com>
2018-07-29 17:40:15 -0500

    Trying to protect the schema changes watching function with the mutex - WIP (errors on MSW)

--- dbhandler/newtablehandler.cpp
+++ dbhandler/newtablehandler.cpp
@@ -1,5 +1,8 @@
 #include <string>
 #include <algorithm>
+#ifndef WIN32
+#include <mutex>
+#endif
 #include "wx/thread.h"
 #include "database.h"
 #include "newtablehandler.h"
@@ -18,7 +21,18 @@ wxThread::ExitCode NewTableHandler::Entry()
     std::vector<std::wstring> errorMsg;
     while( !TestDestroy() )
     {
+#ifdef WIN32
+        m_db->my_mutex.Lock();
+#else
+		{
+            std::lock_guard<std::mutex> locker( m_db->my_mutex );
+#endif
         m_db->NewTableCreation( errorMsg );
+#ifdef WIN32
+        m_db->my_mutex.Unlock();
+#else
+		}
+#endif
         Sleep( 5000 );
     }
     return (wxThread::ExitCode) 0;
