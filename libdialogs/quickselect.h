6b28fbdaa8da24d96f5e105f92f9bdf57e316889
oneeyeman1 <ikorot01@gmail.com>
2019-05-12 11:17:28 -0500

    Should've been part of the previous commit

--- libdialogs/quickselect.h
+++ libdialogs/quickselect.h
@@ -5,6 +5,7 @@ public:
     QuickSelect(wxWindow *parent, const Database *db);
     ~QuickSelect();
     std::vector<Field *> &GetQueryFields();
+    DatabaseTable *GetDatabaseTable();
     const wxListBox *GetQueryTable();
     void OnOkEnableUI(wxUpdateUIEvent &event);
     void OnAddAllUpdateUI(wxUpdateUIEvent &event);
@@ -21,6 +22,8 @@ protected:
     void OnCellLeftClicked(wxGridEvent &event);
     void OnFieldsSetFocus(wxCommandEvent &event);
     void OnOkButton(wxCommandEvent &event);
+    void OnSortClosing(wxCommandEvent &event);
+//    void OnSortClosing(wxGridEvent &event);
 private:
     wxPanel *m_panel;
     wxStaticText *m_label1, *m_label2, *m_label3, *m_label4, *m_label5, *m_label6, *m_label7, *m_comments;
@@ -29,7 +32,8 @@ private:
     wxGrid *m_grid;
     wxButton *m_ok, *m_cancel, *m_addAll, *m_help;
     Database *m_db;
+    DatabaseTable *m_table;
     std::vector<Field *> m_tableFields, m_queryFields;
     wxBoxSizer *m_sizer10;
-    int m_cols;
+    int m_cols, m_column, m_oldColumn;
 };
