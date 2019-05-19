982399ab786ab1ec5ee3086d39c762301161e62a
oneeyeman1 <ikorot01@gmail.com>
2019-05-19 09:59:07 -0500

    Properly create a divider shape - still needs to investigate an up arrow painting

--- libdbwindow/divider.cpp
+++ libdbwindow/divider.cpp
@@ -52,37 +52,47 @@ Divider::Divider() : wxSFRectShape()
     }
 }
 
-Divider::Divider(const wxString &text)
+Divider::Divider(const wxString &text) : wxSFRectShape()
 {
+    wxString upArrow( L"\x2191" );
     AddStyle( sfsLOCK_CHILDREN );
     AcceptChild( "GridShape" );
     m_grid = new wxSFGridShape;
-    if( m_grid )
+    m_text = new wxSFTextShape;
+    m_arrow = new wxSFTextShape;
+    if( m_grid && m_text && m_arrow )
     {
         m_grid->SetRelativePosition( 0, 1 );
         m_grid->SetStyle( sfsALWAYS_INSIDE | sfsPROCESS_DEL |sfsPROPAGATE_DRAGGING | sfsPROPAGATE_SELECTION | sfsLOCK_CHILDREN );
-        m_grid->SetDimensions( 1, 1 );
+        m_grid->SetDimensions( 1, 2 );
+        m_grid->SetCellSpace( 2 );
         m_grid->SetFill( *wxTRANSPARENT_BRUSH );
         m_grid->SetBorder( *wxTRANSPARENT_PEN );
         m_grid->AcceptChild( wxT( "wxSFTextShape" ) );
-        m_grid->AcceptChild( wxT( "wxSFBitmapShape" ) );
         m_grid->Activate( false );
         SF_ADD_COMPONENT( m_grid, wxT( "grid" ) );
-        m_text = new wxSFTextShape;
-        if( m_text )
+        m_text->SetHAlign( wxSFShapeBase::halignLEFT );
+        m_arrow->SetHAlign( wxSFShapeBase::halignLEFT );
+        m_text->SetVAlign( wxSFShapeBase::valignMIDDLE );
+        m_arrow->SetVAlign( wxSFShapeBase::valignMIDDLE );
+        auto font = m_text->GetFont();
+        font.SetWeight( wxFONTWEIGHT_BOLD );
+        m_text->SetFont( font );
+        m_arrow->SetFont( font );
+        m_text->SetStyle( sfsALWAYS_INSIDE | sfsPROCESS_DEL |sfsPROPAGATE_DRAGGING | sfsPROPAGATE_SELECTION | sfsLOCK_CHILDREN );
+        m_arrow->SetStyle( sfsALWAYS_INSIDE | sfsPROCESS_DEL |sfsPROPAGATE_DRAGGING | sfsPROPAGATE_SELECTION | sfsLOCK_CHILDREN );
+        if( m_grid->AppendToGrid( m_text ) )
         {
-            m_text->SetHAlign( wxSFShapeBase::halignCENTER );
-            m_text->SetVAlign( wxSFShapeBase::valignMIDDLE );
-            auto font = m_text->GetFont();
-            font.SetWeight( wxFONTWEIGHT_BOLD );
-            m_text->SetFont( font );
-            m_text->SetStyle( sfsALWAYS_INSIDE | sfsPROCESS_DEL |sfsPROPAGATE_DRAGGING | sfsPROPAGATE_SELECTION | sfsLOCK_CHILDREN );
-            if( m_grid->AppendToGrid( m_text ) )
-                m_text->SetText( text );
+            m_text->SetText( text );
+            if( m_grid->AppendToGrid( m_arrow ) )
+                m_arrow->SetText( upArrow );
             else
-                delete m_text;
+                delete m_arrow;
         }
+        else
+            delete m_text;
     }
+    SetRectSize( 1000, -1 );
 }
 
 Divider::~Divider()
@@ -93,10 +103,15 @@ void Divider::DrawNormal (wxDC &dc)
 {
     wxRect rect = GetBoundingBox();
     dc.SetBrush( *wxGREY_BRUSH );
-    dc.DrawRectangle( 1, rect.y, 1000, rect.y );
+    dc.DrawRectangle( 1, rect.y, 5000, rect.y );
 }
 
 void Divider::DrawSelected(wxDC &dc)
+{
+    DrawNormal( dc );
+}
+
+void Divider::DrawHover(wxDC &dc)
 {
     DrawNormal( dc );
 }
--- libdbwindow/divider.h
+++ libdbwindow/divider.h
@@ -8,8 +8,8 @@ public:
 protected:    
     virtual void DrawNormal(wxDC &dc) wxOVERRIDE;
     virtual void DrawSelected(wxDC &dc) wxOVERRIDE;
+    virtual void DrawHover(wxDC &dc) wxOVERRIDE;
 private:
     wxSFGridShape *m_grid;
-    wxSFTextShape *m_text;
-    wxSFBitmapShape *m_arrow;
+    wxSFTextShape *m_text, *m_arrow;
 };
