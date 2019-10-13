7fa7de9e43b1b2b99a9363cb3e0c8f876a12773a
oneeyeman1 <ikorot01@gmail.com>
2019-10-13 01:03:02 -0500

    Adding font combo box implementation for the query toolbar

---
+++ libdbwindow/fontcombobox.cpp
@@ -0,0 +1,17 @@
+// For compilers that support precompilation, includes "wx/wx.h".
+#include "wx/wxprec.h"
+
+#ifdef __BORLANDC__
+#pragma hdrstop
+#endif
+
+#ifndef WX_PRECOMP
+#include "wx/wx.h"
+#endif
+
+#include "fontcmbobx.h"
+
+FontComboBox::FontComboBox (wxWindow *parent) : wxComboBox (parent, wxID_ANY)
+{
+
+}
---
+++ libdbwindow/fontcombobox.h
@@ -0,0 +1,7 @@
+#pragma once
+
+class FontComboBox : public wxComboBox
+{
+public:
+    FontComboBox(wxWindow *parent);
+};
