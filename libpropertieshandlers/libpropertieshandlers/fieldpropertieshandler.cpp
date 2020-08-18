e4a1f01b04ee972c8b6c14cf29a8f5a19d0af0b8
oneeyeman1 <ikorot01@gmail.com>
2020-08-18 16:24:31 -0500

    Move the files as appropriate

--- libpropertieshandlers/libpropertieshandlers/fieldpropertieshandler.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  fieldpropertieshandler.cpp
-//  libpropertieshandlers
-//
-//  Created by Igor Korot on 8/18/20.
-//  Copyright © 2020 Igor Korot. All rights reserved.
-//
-
-#include "fieldpropertieshandler.hpp"
