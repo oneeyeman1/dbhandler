f1ef6a5c2f8c86dc32743cdb0e3dce4dc50bace1
oneeyeman1 <ikorot01@gmail.com>
2022-01-05 23:06:48 -0600

    Set appropriate grid line color

--- libpropertypages/wherehavingpage.cpp
+++ libpropertypages/wherehavingpage.cpp
@@ -88,6 +88,7 @@ WhereHavingPage::~WhereHavingPage(void)
 void WhereHavingPage::set_properties()
 {
     m_grid->CreateGrid( 10, 4 );
+    m_grid->SetGridLineColour( *wxBLACK );
     m_grid->HideRowLabels();
     m_grid->SetColLabelValue( 0, _( "Column" ) );
     m_grid->SetColLabelValue( 1, _( "Operator" ) );
