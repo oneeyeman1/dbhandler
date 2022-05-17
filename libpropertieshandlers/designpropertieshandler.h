de06dc7dfd7bba1582f088105b461cdd3ab9b542
oneeyeman1 <ikorot01@gmail.com>
2022-05-17 00:02:25 -0500

    Remove compile time dependency on the DBWindow library

--- libpropertieshandlers/designpropertieshandler.h
+++ libpropertieshandlers/designpropertieshandler.h
@@ -3,12 +3,11 @@
 class WXEXPORT DesignPropertiesHander : public PropertiesHandler
 {
 public:
-    DesignPropertiesHander(DesignCanvas *canvas);
+    DesignPropertiesHander(DesignOptions canvas);
     virtual ~DesignPropertiesHander() { }
     virtual void EditProperies(wxNotebook *parent) wxOVERRIDE;
     virtual int GetProperties(std::vector<std::wstring> &errors) wxOVERRIDE;
 private:
-    DesignCanvas *m_canvas;
     DesignOptions m_options;
     DesignGeneral *m_page1;
     PointerPropertiesPanel *m_page2;
