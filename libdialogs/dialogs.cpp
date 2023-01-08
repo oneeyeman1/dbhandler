50b8c1bdfedd91e6d2f5fabec625a8e6d0f5cc9a
oneeyeman1 <ikorot01@gmail.com>
2023-01-08 08:57:17 -0600

    Silence warning on Xcode/clang

--- libdialogs/dialogs.cpp
+++ libdialogs/dialogs.cpp
@@ -409,7 +409,7 @@ extern "C" WXEXPORT int GotoLine(wxWindow *parent, int &lineNo)
 
 extern "C" WXEXPORT int AttachToDatabase(wxWindow *parent, Database *db)
 {
-    int result;
+    int result = 0;
     std::vector<std::wstring> errorMsg;
 #ifdef __WXMSW__
     wxTheApp->SetTopWindow( parent );
