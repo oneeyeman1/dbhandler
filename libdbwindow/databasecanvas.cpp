5cafdfc3c413ff11e0ec5190f87fabbfeb19869f
oneeyeman1 <ikorot01@gmail.com>
2023-02-11 14:23:55 -0600

    Adding saving/restoring sorting fields

--- libdbwindow/databasecanvas.cpp
+++ libdbwindow/databasecanvas.cpp
@@ -78,7 +78,7 @@ QueryRoot::QueryRoot()
     XS_SERIALIZE( m_query, "query" );
     XS_SERIALIZE( m_tables, "query_tables" );
     XS_SERIALIZE( m_fields, "query_fields" );
-    XS_SERIALIZE( m_sortSource, "sort_source" );
+    XS_SERIALIZE( m_sortDest, "sort_dest" );
     XS_SERIALIZE( m_arguments, "query_arguments" );
 }
 
@@ -90,14 +90,14 @@ QueryRoot::QueryRoot(const QueryRoot &root)
     m_tables = root.m_tables;
     m_fields = root.m_fields;
     m_arguments = root.m_arguments;
-    m_sortSource = root.m_sortSource;
+    m_sortDest = root.m_sortDest;
     
     XS_SERIALIZE( m_dbName, "database_name" );
     XS_SERIALIZE( m_dbType, "database_type" );
     XS_SERIALIZE( m_query, "query" );
     XS_SERIALIZE( m_tables, "query_tables" );
     XS_SERIALIZE( m_fields, "query_fields" );
-    XS_SERIALIZE( m_sortSource, "sort_source" );
+    XS_SERIALIZE( m_sortDest, "sort_dest" );
     XS_SERIALIZE( m_arguments, "query_arguments" );
 }
 
@@ -1389,7 +1389,29 @@ void DatabaseCanvas::LoadQuery(const std::map<std::wstring, std::vector<Database
             }
         }
     }
+    for( auto str : dynamic_cast<QueryRoot *>( m_pManager.GetRootItem() )->GetSortDest() )
+    {
+        auto pos = 0;
+        auto found = false;
+        wxStringTokenizer tokens( str );
+        wxVector<wxVariant> data;
+        auto name = tokens.GetNextToken();
+        data.push_back( name );
+        for( ; pos < dynamic_cast<DrawingView *>( m_view )->GetSortPage()->GetSortSourceList()->GetItemCount() && !found; ++pos )
+        {
+            if( dynamic_cast<DrawingView *>( m_view )->GetSortPage()->GetSortSourceList()->GetTextValue( pos, 0 ) == name )
+            {
+                dynamic_cast<DrawingView *>( m_view )->GetSortPage()->GetSortSourceList()->DeleteItem( pos );
+                found = true;
+            }
+        }
+        if( wxAtoi( tokens.GetNextToken() ) == 1 )
+            data.push_back( (wxVariant)  true );
+        else
+            data.push_back( (wxVariant)  false );
+        dynamic_cast<DrawingView *>( m_view )->GetSortPage()->GetSourceDestList()->AppendItem( data, pos );
     }
+}
 
 void DatabaseCanvas::SetQueryArguments(const std::vector<QueryArguments> arguments)
 {
