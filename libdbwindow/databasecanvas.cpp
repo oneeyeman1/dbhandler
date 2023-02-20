151daf722ef437543b1f432051b9f085f9474836
oneeyeman1 <ikorot01@gmail.com>
2023-02-20 13:50:42 -0600

    Warning fix

--- libdbwindow/databasecanvas.cpp
+++ libdbwindow/databasecanvas.cpp
@@ -1299,7 +1299,6 @@ void DatabaseCanvas::LoadQuery(const std::map<std::wstring, std::vector<Database
         {
             for( auto it2 = (*it1).second.begin(); it2 < (*it1).second.end() && !found; ++it2 )
             {
-                auto dbTable = table->GetTable();
                 if( m_dbType == L"SQLite" )
                 {
                     if( table->GetSchemaName() == (*it2)->GetSchemaName() && table->GetTableName() == (*it2)->GetTableName() )
@@ -1357,7 +1356,6 @@ void DatabaseCanvas::LoadQuery(const std::map<std::wstring, std::vector<Database
         {
             for( auto it2 = (*it1).second.begin(); it2 < (*it1).second.end() && !found; ++it2 )
             {
-                auto dbTable = table->GetTable();
                 if( m_dbType == L"SQLite" )
                 {
                     if( table->GetSchemaName() == (*it2)->GetSchemaName() && table->GetTableName() == (*it2)->GetTableName() )
