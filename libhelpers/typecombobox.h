cf5a8f386b404c7b0373790009c198549a991611
Igor Korot <ikorot01@gmail.com>
2023-12-05 01:59:01 -0600

    Adding helper classes library

---
+++ libhelpers/typecombobox.h
@@ -0,0 +1,6 @@
+#pragma once
+class TypeComboBox : public wxComboBox
+{
+public:
+    TypeComboBox(wxWindow *parent, const std::wstring &type, const std::wstring &subtype, const std::wstring &argType);
+};
