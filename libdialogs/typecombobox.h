30e8098ee2fddf876d9f55a2825e9e69f9410943
oneeyeman1 <ikorot01@gmail.com>
2023-12-05 18:54:10 -0600

    Adding helper classes library to OSX build

--- libdialogs/typecombobox.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-class TypeComboBox : public wxComboBox
-{
-public:
-    TypeComboBox(wxWindow *parent, const std::wstring &type, const std::wstring &subtype, const std::string &argType);
-};
