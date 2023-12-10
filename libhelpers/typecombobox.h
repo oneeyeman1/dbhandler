879dace2dbefe8c6713b9f2784275a689522f7e9
oneeyeman1 <ikorot01@gmail.com>
2023-12-10 16:10:31 -0600

    Fix adding the new library

--- libhelpers/typecombobox.h
+++ libhelpers/typecombobox.h
@@ -1,5 +1,11 @@
 #pragma once
-class WXEXPORT TypeComboBox : public wxComboBox
+#ifdef BUILDING_HELPERS
+#define HELPERS_EXPORT __declspec(dllexport)
+#else
+#define HELPERS_EXPORT __declspec(dllimport)
+#endif
+
+class HELPERS_EXPORT TypeComboBox : public wxComboBox
 {
 public:
     TypeComboBox(wxWindow *parent, const std::wstring &type, const std::wstring &subtype, const std::wstring &argType);
