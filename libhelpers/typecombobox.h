7f76291e1f77222579982ca0380624d3ea28761c
oneeyeman1 <ikorot01@gmail.com>
2023-12-13 22:02:52 -0600

    Trying to add a custom control library

--- libhelpers/typecombobox.h
+++ libhelpers/typecombobox.h
@@ -1,6 +1,12 @@
 #pragma once
 
-class TypeComboBox : public wxComboBox
+#ifdef HELPERS_EXPORT
+#define HELPERS_EXPORT __declspec(dllexport)
+#else
+#define HELPERS_EXPORT __declspec(dllimport)
+#endif
+
+class HELPERS_EXPORT TypeComboBox : public wxComboBox
 {
 public:
     TypeComboBox(wxWindow *parent, const std::wstring &type, const std::wstring &subtype, const std::wstring &argType);
