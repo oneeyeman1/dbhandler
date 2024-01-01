cbfcc99ecc49ef5e44a76385384ba37f739c7a29
oneeyeman1 <ikorot01@gmail.com>
2024-01-01 14:40:34 -0600

    Fix toolbar positions for Query View

--- libdbwindow/databaseview.cpp
+++ libdbwindow/databaseview.cpp
@@ -762,14 +762,14 @@ void DrawingView::LayoutChildren(const wxSize &size)
         switch( m_tbSetup[1].m_orientation )
         {
         case 0:
-            m_styleBar->SetSize( 0, 0,  wxDefaultCoord, size.y );
+            m_styleBar->SetSize( offset, 0,  wxDefaultCoord, size.y );
             if( m_tbSetup[0].m_orientation == 0 )
                 offset += m_styleBar->GetSize().x;
             posFrame.x = offset;
             sizeFrame.SetWidth( sizeFrame.GetWidth() - posFrame.x );
             break;
         case 1:
-            m_styleBar->SetSize( 0, 0,  size.x, wxDefaultCoord );
+            m_styleBar->SetSize( 0, offset,  size.x, wxDefaultCoord );
             if( m_tbSetup[0].m_orientation == 1 )
                 offset += m_styleBar->GetSize().y;
             posFrame.y = offset;
