877b222e5662e29c60ba0f1d81c24c51ee4704c8
oneeyeman1 <ikorot01@gmail.com>
2024-01-08 22:03:41 -0600

    This class need handles

--- libdbwindow/designlabel.cpp
+++ libdbwindow/designlabel.cpp
@@ -33,7 +33,7 @@ DesignLabel::DesignLabel() : wxSFRectShape()
     if( m_grid )
     {
         m_grid->SetRelativePosition( 0, 1 );
-        m_grid->SetStyle( sfsALWAYS_INSIDE | sfsPROCESS_DEL | sfsPROPAGATE_DRAGGING | sfsPROPAGATE_SELECTION | sfsLOCK_CHILDREN );
+        m_grid->SetStyle( sfsALWAYS_INSIDE | sfsPROCESS_DEL | sfsPROPAGATE_DRAGGING | sfsPROPAGATE_SELECTION | sfsLOCK_CHILDREN | sfsSHOW_HANDLES );
         m_grid->SetDimensions( 1, 1 );
         m_grid->SetFill( *wxTRANSPARENT_BRUSH );
         m_grid->SetBorder( *wxTRANSPARENT_PEN );
@@ -64,7 +64,7 @@ DesignLabel::DesignLabel(const wxFont font, const wxString &label, int alignment
     if( m_grid && m_text )
     {
         m_grid->SetRelativePosition( 0, 1 );
-        m_grid->SetStyle( sfsALWAYS_INSIDE | sfsPROCESS_DEL | sfsPROPAGATE_DRAGGING | sfsPROPAGATE_SELECTION | sfsLOCK_CHILDREN );
+        m_grid->SetStyle( sfsALWAYS_INSIDE | sfsPROCESS_DEL | sfsPROPAGATE_DRAGGING | sfsPROPAGATE_SELECTION | sfsLOCK_CHILDREN | sfsSHOW_HANDLES );
         m_grid->SetDimensions( 1, 1 );
         m_grid->SetFill( *wxTRANSPARENT_BRUSH );
 //        m_grid->SetBorder( *wxTRANSPARENT_PEN );
@@ -146,4 +146,5 @@ void DesignLabel::Select(bool selected)
 {
     m_fSelected = selected;
     m_text->Select( selected );
+    Select( selected );
 }
