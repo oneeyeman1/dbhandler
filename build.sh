e75cbf1d20a46fc041c776253f26d7f321197b09
Igor Korot <ikorot01@gmail.com>
2024-04-21 20:31:15 -0500

    Modify the build script adding the actual make command

--- build.sh
+++ build.sh
@@ -2,3 +2,4 @@ autoreconf -i
 mkdir Debug
 cd Debug
 ../configure --enable-maintainer-mode 'CFLAGS=-g -O0' 'CXXFLAGS=-g -O0' 'JFLAGS=-g -O0' 'FFLAGS=-g -O0' --with-wx-config=/home/igor/wxWidgets/buildGTK/wx-config
+make j3
