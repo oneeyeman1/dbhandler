c3a66b54295e65549b6403a0ed58d5d295625ce8
Igor Korot <ikorot01@gmail.com>
2024-07-07 23:59:18 -0500

    Fix configure line

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@ rm -rf Debug
 autoreconf -i
 mkdir Debug
 cd Debug
-../configure --enable-maintainer-mode 'CFLAGS=-g -O0' 'CXXFLAGS=-g -O0' 'JFLAGS=-g -O0' 'FFLAGS=-g -O0' --with-wx-config=/home/igor/wxWidgets/buildGTK/wx-config
+../configure --enable-maintainer-mode 'CFLAGS=-g -O0' 'CXXFLAGS=-g -O0' 'JFLAGS=-g -O0' 'FFLAGS=-g -O0' --with-wx-config
 make -j3
