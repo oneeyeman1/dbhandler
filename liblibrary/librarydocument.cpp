61ff702218bdea234838677f4472647a9cf9ad61
oneeyeman1 <ikorot01@gmail.com>
2024-08-11 00:16:05 -0500

    Start implementing the library document

--- liblibrary/librarydocument.cpp
+++ liblibrary/librarydocument.cpp
@@ -1,2 +1,29 @@
+// For compilers that support precompilation, includes "wx/wx.h".
+#include "wx/wxprec.h"
+
+#ifdef __BORLANDC__
+#pragma hdrstop
+#endif
+#include <string>
+#if !wxUSE_DOC_VIEW_ARCHITECTURE
+#error You must set wxUSE_DOC_VIEW_ARCHITECTURE to 1 in setup.h!
+#endif
+
+#ifndef WX_PRECOMP
+#include "wx/wx.h"
+#endif
+
 #include "wx/docview.h"
 #include "librarydocument.h"
+
+// ----------------------------------------------------------------------------
+// DrawingDocument implementation
+// ----------------------------------------------------------------------------
+
+wxIMPLEMENT_DYNAMIC_CLASS(LibraryDocument, wxDocument);
+
+LibraryDocument::~LibraryDocument()
+{
+//    m_successfulLoad = false;
+}
+
--- liblibrary/librarydocument.h
+++ liblibrary/librarydocument.h
@@ -1,4 +1,9 @@
 #pragma once
 class LibraryDocument :  public wxDocument
 {
+public:
+    LibraryDocument() : wxDocument() { }
+    ~LibraryDocument();
+private:
+    wxDECLARE_DYNAMIC_CLASS(LibraryDocument);
 };
