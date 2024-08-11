8d2eef897ddfabc2208522ffda733c9e0678b318
oneeyeman1 <ikorot01@gmail.com>
2024-08-11 11:32:13 -0500

    Start implementing library view

--- liblibrary/libraryview.cpp
+++ liblibrary/libraryview.cpp
@@ -16,11 +16,49 @@
 #include <map>
 #include "wx/docview.h"
 #include "wx/docmdi.h"
+#include "wx/stdpaths.h"
+#include "wx/filename.h"
 #include "configuration.h"
 #include "librarydocument.h"
 #include "libraryview.h"
 
+wxIMPLEMENT_DYNAMIC_CLASS(LibraryViewPainter, wxView);
+
 bool LibraryViewPainter::OnCreate(wxDocument *doc, long flags)
 {
+    wxToolBar *tb = nullptr;
+    wxWindowList children;
+    if( !wxView::OnCreate( doc, flags ) )
+        return false;
+#ifndef __WXMSW__
+    children = m_parent->GetChildren();
+#else
+    children = m_parent->GetClientWindow()->GetChildren();
+#endif
+    auto found = false;
+    for( wxWindowList::iterator it = children.begin(); it != children.end() && !found; it++ )
+    {
+        tb = wxDynamicCast( *it, wxToolBar );
+        if( tb && tb->GetName() == "ViewBar" )
+        {
+            found = true;
+            m_tb = tb;
+        }
+    }
+    auto stdPath = wxStandardPaths::Get();
+#ifdef __WXOSX__
+    wxFileName fn( stdPath.GetExecutablePath() );
+    fn.RemoveLastDir();
+    m_libPath = fn.GetPathWithSep() + "Frameworks/";
+#elif __WXGTK__ || __WXGTK__
+    m_libPath = stdPath.GetInstallPrefix() + "/lib/";
+#elif __WXMSW__
+    wxFileName fn( stdPath.GetExecutablePath() );
+    m_libPath = fn.GetPathWithSep();
+#endif
     return true;
+}
+
+void LibraryViewPainter::OnDraw( wxDC * )
+{
 }
--- liblibrary/libraryview.h
+++ liblibrary/libraryview.h
@@ -4,6 +4,7 @@ class LibraryViewPainter :  public wxView
 {
 public:
     virtual bool OnCreate(wxDocument *doc, long flags) wxOVERRIDE;
+    virtual void OnDraw(wxDC *) wxOVERRIDE;
     void SetViewType(ViewType type) { m_type = type; }
     void SetParentWindow(wxWindow *window) { m_parent = wxStaticCast( window, wxDocMDIParentFrame ); }
     void SetConfiguration(Configuration *conf) { m_conf = conf; }
@@ -11,4 +12,7 @@ private:
     ViewType m_type;
     wxDocMDIParentFrame *m_parent;
     Configuration *m_conf;
+    wxToolBar *m_tb;
+    wxString m_libPath;
+    wxDECLARE_DYNAMIC_CLASS(LibraryViewPainter);
 };
