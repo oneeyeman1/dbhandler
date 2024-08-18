f4e7c6b785671bc0b91b1db75f9136a489895ec4
oneeyeman1 <ikorot01@gmail.com>
2024-08-17 19:42:15 -0500

    Adding new IDs

--- libtablewindow/tablecanvas.h
+++ libtablewindow/tablecanvas.h
@@ -130,6 +130,7 @@ private:
 #define wxID_PASTECOLUMN          503
 #define wxID_INSERTCOLUMN         504
 #define wxID_DELETECOLUMN         505
+#define wxID_LIBRARYNEW           601
 #define wxID_LEFTALIGN            1000
 #define wxID_CENTERALIGN          1001 
 #define wxID_RIGHTALIGN           1002
