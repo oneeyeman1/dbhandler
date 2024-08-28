2e9ed6d59c520edcac5f29f3a21a1e9a056aa5ed
oneeyeman1 <ikorot01@gmail.com>
2024-08-27 21:53:14 -0500

    Added function to read/write lbrary object

--- liblibrary/librarydocument.cpp
+++ liblibrary/librarydocument.cpp
@@ -14,6 +14,11 @@
 #endif
 
 #include "wx/docview.h"
+#if wxUSE_STD_IOSTREAM
+#include "wx/ioswrap.h"
+#else
+#include "wx/txtstrm.h"
+#endif
 #include "painterobjects.h"
 #include "librarydocument.h"
 
@@ -26,5 +31,57 @@ wxIMPLEMENT_DYNAMIC_CLASS(LibraryDocument, wxDocument);
 LibraryDocument::~LibraryDocument()
 {
 //    m_successfulLoad = false;
+}
+
+DocumentOstream &LibraryDocument::SaveObject(DocumentOstream &ostream)
+{
+#if wxUSE_STD_IOSTREAM
+    DocumentOstream &stream = ostream;
+#else
+    wxTextOutputStream stream( ostream );
+#endif
+//    m_querySaveSuccessfl = ((DrawingView *) GetFirstView() )->GetDatabaseCanvas()->GetDiagramManager().SerializeToXml( GetFilename(), xsWITH_ROOT );
+    wxDocument::SaveObject( stream );
+
+    return ostream;
+}
+
+DocumentIstream &LibraryDocument::LoadObject(DocumentIstream &istream)
+{
+#if wxUSE_STD_IOSTREAM
+    DocumentIstream &stream = istream;
+#else
+    wxTextInputStream stream( istream );
+#endif
+    std::vector<std::wstring> errors;
+/*    auto result = ((DrawingView *) GetFirstView() )->GetDatabaseCanvas()->GetDiagramManager().DeserializeFromXml( GetFilename() );
+    if( result )
+    {
+        auto tables = dynamic_cast<QueryRoot *>( ((DrawingView *) GetFirstView() )->GetDatabaseCanvas()->GetDiagramManager().GetRootItem() )->GetTables();
+        for( auto name : tables )
+        {
+            wxString catalog, schema, table;
+            if( m_db->GetTableVector().m_type == L"SQLite" )
+            {
+                catalog = "";
+                schema = name.substr( 0, name.find( '.' ) );
+                table = name.substr( name.find( '.' ) + 1 );
+            }
+            else
+            {
+                catalog = name.substr( 0, name.find( '.' ) );
+                schema = name.substr( name.find( '.' ) + 1, name.rfind( '.' ) );
+                table = name.substr( name.rfind( '.' ) );
+            }
+            m_tableNames.push_back( table.ToStdWstring() );
+            if( m_db->AddDropTable( catalog.ToStdWstring(), schema.ToStdWstring(), table.ToStdWstring(), errors ) )
+                for( std::vector<std::wstring>::iterator it = errors.begin(); it < errors.end(); ++it )
+                    wxMessageBox( (*it) );
+        }
+        m_successfulLoad = true;
+    }
+*/
+    wxDocument::LoadObject( stream );
+    return istream;
 }
 
--- liblibrary/librarydocument.h
+++ liblibrary/librarydocument.h
@@ -1,11 +1,25 @@
 #pragma once
-
+// This sample is written to build both with wxUSE_STD_IOSTREAM==0 and 1, which
+// somewhat complicates its code but is necessary in order to support building
+// it under all platforms and in all build configurations
+//
+// In your own code you would normally use std::stream classes only and so
+// wouldn't need these typedefs
+#if wxUSE_STD_IOSTREAM
+typedef wxSTD istream DocumentIstream;
+typedef wxSTD ostream DocumentOstream;
+#else // !wxUSE_STD_IOSTREAM
+typedef wxInputStream DocumentIstream;
+typedef wxOutputStream DocumentOstream;
+#endif // wxUSE_STD_IOSTREAM/!wxUSE_STD_IOSTREAM
 
 class LibraryDocument :  public wxDocument
 {
 public:
     LibraryDocument() : wxDocument() { }
     ~LibraryDocument();
+    DocumentOstream& SaveObject(DocumentOstream& stream) wxOVERRIDE;
+    DocumentIstream& LoadObject(DocumentIstream& stream) wxOVERRIDE;
 private:
     LibraryObject m_libraryObject;
     wxDECLARE_DYNAMIC_CLASS(LibraryDocument);
