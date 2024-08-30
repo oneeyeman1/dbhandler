08aa1b6245ea9ae0d13a48a6981d42d390c10519
oneeyeman1 <ikorot01@gmail.com>
2024-08-29 23:40:54 -0500

    Make the objects better reflect the structure

--- libhelpers/painterobjects.h
+++ libhelpers/painterobjects.h
@@ -2,14 +2,15 @@
 
 struct Object
 {
-    wxDateTime m_created, m_modifed;
+    wxDateTime m_compiled, m_modifiied;
     int m_size;
     wxString m_name, m_comment, m_libName, m_checkedOutBy;
+    Object(const wxString &libName, const wxString &name, const wxDateTime &created, const wxDateTime &modified, const wxString &comment) : m_libName( libName ), m_name( name ), m_compiled( created ), m_modifiied( modified ), m_comment( comment ) { }
 };
 
 struct LibraryObject
 {
-    wxDateTime m_created, m_modified;
+    wxDateTime m_created;
     wxString m_comment, m_name;
     std::vector<Object> m_objects;
 };
