a2dfd68b01737b4b08d9c8b17efc048d056019c1
oneeyeman1 <ikorot01@gmail.com>
2024-09-08 14:15:24 -0500

    Add new men id

--- dbhandler/docview.h
+++ dbhandler/docview.h
@@ -175,6 +175,7 @@ wxDECLARE_APP(MyApp);
 #define wxID_DELETECOLUMN         505
 #define wxID_LIBRARYNEW           601
 #define wxID_LIBRARYDELETE        602
+#define wxID_IMPORTLIBRARY        603
 #define wxID_LEFTALIGN            1000
 #define wxID_CENTERALIGN          1001 
 #define wxID_RIGHTALIGN           1002
