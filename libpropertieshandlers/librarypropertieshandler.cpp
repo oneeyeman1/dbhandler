b85cd69c2cbe01a8445bce62f471b11e7a25509a
oneeyeman1 <ikorot01@gmail.com>
2024-10-07 22:37:17 -0500

    Implement saving library object

--- libpropertieshandlers/librarypropertieshandler.cpp
+++ libpropertieshandlers/librarypropertieshandler.cpp
@@ -19,7 +19,7 @@ LibraryPropertiesHandler::LibraryPropertiesHandler(LibraryProperty prop)
     m_properties = prop;
 }
 
-wxAny LibraryPropertiesHandler::GetProperties(std::vector<std::wstring> &errors)
+wxAny LibraryPropertiesHandler::GetProperties(std::vector<std::wstring> &WXUNUSED(errors))
 {
     return m_obj;
 }
@@ -31,10 +31,10 @@ int LibraryPropertiesHandler::ApplyProperties()
     doc.SetRoot( root );
     wxXmlNode *header = new wxXmlNode( root, wxXML_ELEMENT_NODE, "Header" );
     wxXmlNode *libCreated = new wxXmlNode( header, wxXML_ELEMENT_NODE, "Created" );
-    libCreated->AddChild( new wxXmlNode( wxXML_TEXT_NODE, "", library->GetCreationT>
+    libCreated->AddChild( new wxXmlNode( wxXML_TEXT_NODE, "", m_properties.m_created.FormatISOCombined() ) );
     wxXmlNode *comment = new wxXmlNode( header, wxXML_ELEMENT_NODE, "Comment" );
-    comment->AddChild( new wxXmlNode( wxXML_TEXT_NODE, "", library->GetComment() );
-    wxFileOutputStream stream( name );
+    comment->AddChild( new wxXmlNode( wxXML_TEXT_NODE, "", m_properties.m_comment ) );
+    wxFileOutputStream stream( m_name );
     doc.Save( stream );
     return 0;
 }
--- libpropertieshandlers/librarypropertieshandler.h
+++ libpropertieshandlers/librarypropertieshandler.h
@@ -6,7 +6,9 @@ public:
     virtual int ApplyProperties() override;
     virtual wxAny GetProperties(std::vector<std::wstring> &errors) override;
     const LibraryProperty &GetLibraryProperties() const { return m_properties; }
+    void SetLibraryFileName(const wxString &name) { m_name = name; }
 private:
     LibraryProperty m_properties;
+    wxString m_name;
 };
 
