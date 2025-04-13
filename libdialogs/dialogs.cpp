347721b190dd43e7c99af386437eb7f55d137eab
oneeyeman1 <ikorot01@gmail.com>
2025-04-13 12:34:16 -0500

    Remove unneeded DllMain - the default one is sufficient

--- libdialogs/dialogs.cpp
+++ libdialogs/dialogs.cpp
@@ -99,64 +99,6 @@
 #include "createviewoptions.h"
 #include "saveview.h"
 
-#ifdef __WXMSW__
-WXDLLIMPEXP_BASE void wxSetInstance( HINSTANCE hInst );
-
-BOOL APIENTRY DllMain( HANDLE hModule, DWORD fdwReason, LPVOID lpReserved)
-{
-    lpReserved = lpReserved;
-    hModule = hModule;
-#ifndef WXUSINGDLL
-    int argc = 0;
-    char **argv = NULL;
-#endif
-    switch( fdwReason )
-    {
-    case DLL_PROCESS_ATTACH:
-#ifdef WXUSINGDLL
-        wxInitialize();
-#else
-        wxSetInstance( (HINSTANCE) hModule );
-        wxEntryStart( argc, argv );
-        if( !wxTheApp || !wxTheApp->CallOnInit() )
-        {
-            wxEntryCleanup();
-            if( wxTheApp )
-                wxTheApp->OnExit();
-            return FALSE;
-        }
-#endif
-        break;
-    case DLL_PROCESS_DETACH:
-#ifdef WXUSINGDLL
-        wxUninitialize();
-#else
-        if( wxTheApp )
-            wxTheApp->OnExit();
-        wxEntryCleanup();
-#endif
-        break;
-    }
-    return TRUE;
-}
-#endif
-
-class MyDllApp : public wxApp
-{
-public:
-    bool OnInit()
-    {
-        return true;
-    }
-
-    int OnExit()
-    {
-        return 0;
-    }
-};
-
-IMPLEMENT_APP_NO_MAIN(MyDllApp);
-
 extern "C" WXEXPORT void ODBCSetup(wxWindow *pParent)
 {
 #ifdef __WXMSW__
