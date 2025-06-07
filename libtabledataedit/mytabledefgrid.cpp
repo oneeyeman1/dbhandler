3f5cd68f51e9bb5c75dcdb103d57ec9a6bf91942
oneeyeman1 <ikorot01@gmail.com>
2025-06-07 13:11:54 -0400

    Addng handler for changing the cell selection

--- libtabledataedit/mytabledefgrid.cpp
+++ libtabledataedit/mytabledefgrid.cpp
@@ -3,10 +3,19 @@
 
 MyTableDefGrid::MyTableDefGrid(wxWindow *parent, wxWindowID id) : wxGrid( parent, id), m_newRow( 0 )
 {
-
+//    Bind( wxEVT_GRID_CELL_LEFT_CLICK, &MyTableDefGrid::OnCellClicked, this );
+//    Bind( wxEVT_GRID_CELL_LEFT_DCLICK, &MyTableDefGrid::OnCellClicked, this );
+//    Bind( wxEVT_GRID_CELL_RIGHT_CLICK, &MyTableDefGrid::OnCellClicked, this );
+//    Bind( wxEVT_GRID_CELL_RIGHT_DCLICK, &MyTableDefGrid::OnCellClicked, this );
+    Bind( wxEVT_GRID_SELECT_CELL, &MyTableDefGrid::OnCellClicked, this );
 }
 
 void MyTableDefGrid::DrawRowLabel(wxDC &dc, int row)
 {
     wxGrid::DrawRowLabel( dc, row );
+}
+
+void MyTableDefGrid::OnCellClicked(wxGridEvent &event)
+{
+    auto row = event.GetRow();
 }
--- libtabledataedit/mytabledefgrid.h
+++ libtabledataedit/mytabledefgrid.h
@@ -5,6 +5,8 @@ class MyTableDefGrid :  public wxGrid
 public:
     MyTableDefGrid(wxWindow *parent, wxWindowID id);
     virtual void DrawRowLabel(wxDC &dc, int row) wxOVERRIDE;
+protected:
+    void OnCellClicked(wxGridEvent &event);
 private:
     int m_newRow, m_oldRow;
 };
