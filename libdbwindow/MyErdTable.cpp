3276472b876ceb1dbd52c39cd83d494642f24746
oneeyeman1 <ikorot01@gmail.com>
2026-03-19 16:51:22 -0700

    Pass PK options to DB class for processng

--- libdbwindow/MyErdTable.cpp
+++ libdbwindow/MyErdTable.cpp
@@ -574,7 +574,7 @@ int MyErdTable::ApplyProperties(const wxAny &any, bool logOnly, std::wstring &co
         prop.m_labelFontSize != table->GetTableProperties().m_labelFontSize )
         result = m_db->SetTableProperties( GetTable(), prop, logOnly, command, errorMsg );
     if( !result && ( prop.primaryKey != table->GetPKFelds() || prop.pkOptions.get()->notequal( table->GetPKOptions() ) ) )
-        result = m_db->EditPrimaryKey( GetCatalogName().ToStdWstring(), GetSchemaName().ToStdWstring(), GetTableName().ToStdWstring(), prop.primaryKey, logOnly, command, errorMsg );
+        result = m_db->EditPrimaryKey( GetCatalogName().ToStdWstring(), GetSchemaName().ToStdWstring(), GetTableName().ToStdWstring(), prop.primaryKey, prop.pkOptions, logOnly, command, errorMsg );
     if( !result )
     {
         if( !logOnly )
