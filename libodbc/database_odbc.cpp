29a5835623b4ecec569a46cf2b121d5c2e0512aa
oneeyeman1 <ikorot01@gmail.com>
2026-04-27 23:44:11 -0700

    Properly populate the parameter

--- libodbc/database_odbc.cpp
+++ libodbc/database_odbc.cpp
@@ -459,11 +459,13 @@ int ODBCDatabase::CreateDatabase(const std::wstring &name, const std::shared_ptr
             GetErrorMessage( errorMsg, CONN_ERROR );
             result = 1;
         }
+        std::unique_ptr<SQLWCHAR[]> newDBName( new SQLWCHAR[name.length() + 2] );
+        memset( newDBName.get(), '\0', name.length() + 2 );
+        uc_to_str_cpy( newDBName.get(), name );
         if( !result )
         {
-            SQLWCHAR newDBName[name.length() + 2];
             SQLLEN cbDBName = SQL_NTS;
-            ret = SQLBindParameter( m_hstmt, 1, SQL_PARAM_INPUT, SQL_C_WCHAR, SQL_WCHAR, name.length(), 0, newDBName, 0, &cbDBName );
+            ret = SQLBindParameter( m_hstmt, 1, SQL_PARAM_INPUT, SQL_C_WCHAR, SQL_WCHAR, name.length(), 0, newDBName.get(), 0, &cbDBName );
             if( ret != SQL_SUCCESS && ret != SQL_SUCCESS_WITH_INFO )
             {
                 GetErrorMessage( errorMsg, CONN_ERROR );
