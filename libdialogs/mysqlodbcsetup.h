60a2cb81e39659717f071706221b4de134175d40
Igor Korot <ikorot01@gmail.com>
2026-07-26 17:32:32 -0500

    Continue with implementation

--- libdialogs/mysqlodbcsetup.h
+++ libdialogs/mysqlodbcsetup.h
@@ -9,7 +9,8 @@ private:
     wxStaticBitmap *m_logo;
     wxStaticLine *m_line;
     wxStaticText *m_label1, *m_label2, *m_label3, *m_label4, *m_label5, *m_label6, *m_label7;
-    wxTextCtrl *m_name, *m_desc, *m_serverName, *m_port, *m_pipeName, *m_user, *m_password, m_dbName;
+    wxTextCtrl *m_name, *m_desc, *m_serverName, *m_port, *m_pipeName, *m_user, *m_password, *m_dbName;
     wxRadioButton *m_network, *m_pipe;
+    wxButton *m_test;
 };
 
