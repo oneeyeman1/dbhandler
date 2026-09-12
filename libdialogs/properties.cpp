5bfa901ee0b441f3761929886caae7d11be99cdc
Igor Korot <ikorot01@gmail.com>
2026-09-11 23:37:32 -0500

    Gradually fixing compiler error due to switching to unique_ptr from shared_ptr

--- libdialogs/properties.cpp
+++ libdialogs/properties.cpp
@@ -151,15 +151,14 @@ PropertiesDialog::PropertiesDialog(wxWindow* parent, wxWindowID id, const wxStri
         break;
         case DatabaseFieldPropertiesType:
         {
-            FieldProperties prop = ( handler )->GetProperties().As<FieldProperties>();
             auto type = ( handler )->GetFieldType();
-            m_page6 = new FieldGeneral( m_properties, prop.m_comment );
+            m_page6 = new FieldGeneral( m_properties, ( handler )->GetProperties().As<FieldProperties>().m_comment );
             m_properties->AddPage( m_page6, _( "General" ) );
-            m_page7 = new FieldHeader( m_properties, prop.m_heading );
+            m_page7 = new FieldHeader( m_properties, ( handler )->GetProperties().As<FieldProperties>().m_heading );
             m_properties->AddPage( m_page7, _( "Headers" ) );
-            m_page8 = new DatabaseFieldDisplay( m_properties, prop.m_display, handler->GetFieldType(), handler->GetDatabase() );
+            m_page8 = new DatabaseFieldDisplay( m_properties, ( handler )->GetProperties().As<FieldProperties>().m_display, handler->GetFieldType(), handler->GetDatabase() );
             m_properties->AddPage( m_page8, _( "Display" ) );
-            m_page18 = new FieldValidation( m_properties, prop.m_validations, ( handler )->GetDatabase(), type );
+            m_page18 = new FieldValidation( m_properties, ( handler )->GetProperties().As<FieldProperties>().m_validations, ( handler )->GetDatabase(), type );
             m_properties->AddPage( m_page18, _( "Validation" ) );
             m_page17 = new FieldStyles( m_properties );
             m_properties->AddPage( m_page17, _( "Edit Style" ) );
@@ -286,7 +285,7 @@ bool PropertiesDialog::ApplyProperties()
             FieldProperties prop;
             logOnly = m_page6->IsLogOnly();
             prop.m_comment = m_page6->GetCommentCtrl()->GetValue();
-            any = prop;
+            any = std::move( prop );
         }
         break;
         case DatabaseTablePropertiesType:
